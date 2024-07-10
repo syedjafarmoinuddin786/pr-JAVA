309243b49a4d1e01079c0c82eb9c560db6e2b394
SJM8786 <149411284+syedjafarmoinuddin786@users.noreply.github.com>
2024-07-10 12:02:54 +0530

    Add files via upload

---
+++ largest or smallest of two numbers.java
@@ -0,0 +1,18 @@
+// COMPARING TWO NUMBERS WHICH ONE IS GREATER & WHICH ONE IS SMALL
+public class bs{
+	public static void main(String[] args){
+				int a=25,b=16;
+				if(a>b){
+					System.out.println("A IS BIG");
+				}
+				else if(b>a){
+					System.out.println("B IS BIG");
+				}
+				else if(a==b){
+					System.out.println("both ARE equal\n\t");
+				}
+				else{
+					System.out.println("INVALID EXP");
+				}
+	}
+}
