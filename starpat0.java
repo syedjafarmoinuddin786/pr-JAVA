a91dd5f00339605251f55be78744256938848aed
SJM8786 <149411284+syedjafarmoinuddin786@users.noreply.github.com>
2024-07-24 11:36:31 +0530

    Add files via upload

---
+++ starpat0.java
@@ -0,0 +1,13 @@
+public class starpat0{
+	public	static void main(String[] args){
+			System.out.println("STAR PATTERN");
+			int rows = 5;
+			int i,j;
+			for(i=0;i<5;i++){
+				for(j=1;j<=i;j++){
+					System.out.print("*");
+				}
+				System.out.println("*");
+			}	
+	}
+}  
